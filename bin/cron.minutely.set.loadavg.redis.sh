d1800ef9308f0f226e7172869fad4f26164eadf5
Evan Summers <evan.summers@gmail.com>
2016-05-24 20:26:43 +0200

    cron

--- bin/cron.minutely.set.loadavg.redis.sh
+++ bin/cron.minutely.set.loadavg.redis.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 minute=`date +%M`
-while [ $minute -eq `date +%M` ]
+while [ `date +%M` -eq $minute ]
 do
   redis-cli setex 'scan:loadavg' 90 `cat /proc/loadavg | cut -d'.' -f1 | grep [0-9]` | grep -v OK
   sleep 13
